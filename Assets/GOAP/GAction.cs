2cbbe40e5db8925720b841c77eb7852a6712d264
Vitor-Borges <62852982+Vitor-Borges@users.noreply.github.com>
2022-12-28 07:58:50 +0000

    Implementing the Inventory System
    Etapa Concluida.

--- Assets/GOAP/GAction.cs
+++ Assets/GOAP/GAction.cs
@@ -20,6 +20,7 @@ public abstract class GAction : MonoBehaviour
     public WorldStates agentBeliefs;
 
     public GInventory inventory;
+    public WorldStates beliefs;
 
 
     public bool running = false;
@@ -47,8 +48,10 @@ public abstract class GAction : MonoBehaviour
             }
 
         inventory = this.GetComponent<GAgent>().inventory;
+        beliefs = this.GetComponent<GAgent>().beliefs;
 
-            
+
+
     }
 
     public bool IsAchievable()
--- Assets/Patient.cs
+++ Assets/Patient.cs
@@ -9,7 +9,11 @@ public class Patient : GAgent
     {
         base.Start();
         SubGoal s1 = new SubGoal("isWaiting", 1, true);
-        goals.Add(s1, 3); 
+        goals.Add(s1, 3);
+
+        base.Start();
+        SubGoal s2 = new SubGoal("isTreated", 1, true);
+        goals.Add(s1, 5);
     }
 
 }
